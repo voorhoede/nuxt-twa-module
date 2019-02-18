6e5088077e294eed72c6c5e2a27d1c370cea0e0a
Celine Kurpershoek <celinekurpershoek@gmail.com>
2019-02-18 10:45:38 +0100

    feat(test): add test for build file

---
+++ test/helpers/index.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const fs = require('fs')
+const consola = require('consola')
+
+/** prepare the app folder for the generate build file tests */
+module.exports = function prepareAppFolder (directory) {
+    const originalGradle = path.resolve(__dirname, '../fixture/mockdata/build.gradle')
+    const copiedGradlePath = path.resolve(__dirname, '../test-env/app/build.gradle')
+
+    if(fs.existsSync(directory)){ 
+        rimraf(directory, (error) => {
+            return consola.error(error)
+        })
+    }
+
+    fs.mkdirSync(directory)
+
+    fs.copyFile(originalGradle, copiedGradlePath, (err) => {
+        if (err) return consola.error(err);
+        return consola.success('prepareAppFolder: copied original gradle to test folder');
+    });
+
+}
